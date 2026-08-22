5e098e30118f8274e6fdd00cf25ad0c4698cb8dc
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-22 13:04:27 -0400

    Mount Theme OS alongside Dataarchy shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import DataarchyShell from "@/components/dataarchy-shell";
 import MissionDrawer from "@/components/mission-drawer";
+import ThemeOS from "@/components/theme-os";
 
 export default function Home() {
   return (
     <>
       <DataarchyShell />
       <MissionDrawer />
+      <ThemeOS />
     </>
   );
 }
