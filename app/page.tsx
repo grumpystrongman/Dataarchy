6168d2db07b4590c6602bf3282e54a5cd78d0f05
Jeff Barnes <38039967+grumpystrongman@users.noreply.github.com>
2026-08-22 10:51:41 -0400

    Mount durable mission memory console

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import DataarchyShell from "@/components/dataarchy-shell";
+import MissionDrawer from "@/components/mission-drawer";
 
 export default function Home() {
-  return <DataarchyShell />;
+  return (
+    <>
+      <DataarchyShell />
+      <MissionDrawer />
+    </>
+  );
 }
